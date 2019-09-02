eebe51b1b812e68cd8e6e33c647489bd36a4c56c
WronskiN <norbertwronski@yahoo.com>
2019-09-02 10:39:07 +0100

    modified navigation and added bcg image to product section

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,4 @@
+// NAVIGATION - HAMBURGER
 const btn = document.querySelector('.navigation__button');
 const menuList = document.querySelector('.navigation__list');
 
@@ -5,3 +6,16 @@ btn.addEventListener('click', function() {
   btn.classList.toggle('navigation__active')
   menuList.classList.toggle('navigation__list--mb')
 });
+
+// NAVIGATION - 
+window.onscroll = function() {
+  const navbar = document.getElementById("navigation");
+
+  if (window.pageYOffset >= 100) {
+    navbar.classList.add("sticky")
+    navbar.style.opacity = ".9";
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.style.opacity = "1";
+  }
+}
