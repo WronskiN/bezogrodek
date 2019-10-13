d27183099a6725072673a1bced59fad5a80db11d
WronskiN <norbertwronski@yahoo.com>
2019-10-13 15:29:49 +0100

    CSS - Overflow-x hidded. Javascript - Mobile menu deissapear after click on link

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,39 @@
-// NAVIGATION - HAMBURGER
+/////////////////////////////////////////////////////////////////////
+//////////////// NAVIGATION - HAMBURGER
 const btn = document.querySelector('.navigation__button');
 const menuList = document.querySelector('.navigation__list');
-
 btn.addEventListener('click', function() {
   btn.classList.toggle('navigation__active')
   menuList.classList.toggle('navigation__list--mb')
 });
 
-// NAVIGATION - OPACITY CHANGE ON SCROLL 
+/////////////////////////////////////////////////////////////////////
+///////////////// NAVIGATION FOR MOBILE - HIDE THE NAV MENU AFTER CLICK
+const items = [...document.querySelectorAll('.navigation__item--link')];
+items.forEach(function(e) {
+  e.addEventListener('click', () => {
+    menuList.classList.remove('navigation__list--mb');
+  }, 1000);
+});
+
+/////////////////////////////////////////////////////////////////////
+//////////////// NAVIGATION - OPACITY CHANGE ON SCROLL 
 window.onscroll = function() {
   const navbar = document.getElementById("navigation");
-
   if (window.pageYOffset >= 100) {
     navbar.style.opacity = ".9";
   } else {
     navbar.style.opacity = "1";
   }
 };
-// JQuery trigger animation onscroll
+
+//////////////////////////////////////////////////////////////////////
+////////////// JQuery trigger animation onscroll
 $(document).on('scroll', function() {
   const windowHeight = $(window).height();
   const scrollValue = $(this).scrollTop()
-
   const $sectionProduct = $('.product');
   const $sectionIcon = $('.product__box--icon');
-  
   const sectionFromTop = $sectionProduct.offset().top;
   const sectionHeight = $sectionProduct.outerHeight();
 
@@ -33,7 +42,9 @@ $(document).on('scroll', function() {
   }
 
 });
-// jQuery Smooth Scroll (Nav)
+
+///////////////////////////////////////////////////////////////////////
+//////////////// JQuery Smooth Scroll (Nav)
 $('.navigation a').on('click', function(e) {
   if(this.hash !== '') {
     e.preventDefault();
